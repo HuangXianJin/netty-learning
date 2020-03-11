65aa10bac521c6b105d54f9abd81f8937ce56884
haungxj <914103719@qq.com>
2020-03-11 15:04:19 +0800

    hello world
    Signed-off-by: haungxj <914103719@qq.com>

---
+++ netty-helloworld/src/main/java/com/huangxj/netty/server/ServerHandler.java
@@ -0,0 +1,57 @@
+package com.huangxj.netty.server;
+
+
+import io.netty.channel.*;
+
+import java.net.InetAddress;
+import java.util.Date;
+
+/**
+ * @ClassName ServerHandler
+ * @Description TODO
+ * @Author: huangxj
+ * @Create: 2020-03-11 11:36
+ * @Version V1.0
+ **/
+@ChannelHandler.Sharable
+public class ServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // 为新连接发送庆祝
+        ctx.write("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\r\n");
+        ctx.write("It is " + new Date() + " now.\r\n");
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String request) throws Exception {
+        // Generate and write a response.
+        String response;
+        boolean close = false;
+        if (request.isEmpty()) {
+            response = "Please type something.\r\n";
+        } else if ("bye".equals(request.toLowerCase())) {
+            response = "Have a good day!\r\n";
+            close = true;
+        } else {
+            response = "Did you say '" + request + "'?\r\n";
+        }
+
+        ChannelFuture future = ctx.write(response);
+
+        if (close) {
+            future.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
