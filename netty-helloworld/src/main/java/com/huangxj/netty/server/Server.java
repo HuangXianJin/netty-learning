00a33a8ea78f4a0374c28e192d30e3d2313e5823
HuangXianJin <914103719@qq.com>
2020-03-11 11:13:29 +0800

    测试提交
    Signed-off-by: haungxj <914103719@qq.com>

---
+++ netty-helloworld/src/main/java/com/huangxj/netty/server/Server.java
@@ -0,0 +1,14 @@
+package com.huangxj.netty.server;
+
+/**
+ * @ClassName Server
+ * @Description TODO
+ * @Author: huangxj
+ * @Create: 2020-03-11 11:10
+ * @Version V1.0
+ **/
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
